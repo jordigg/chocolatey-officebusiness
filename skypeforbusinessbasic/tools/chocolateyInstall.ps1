da103594d5a3cc139d4909329d33a6f16d6fc104
Jordi Garcia <jordi@improbable.io>
2017-03-27 16:37:11 +0100

    Update SkypeBussinesBasic

--- skypeforbusinessbasic/tools/chocolateyInstall.ps1
+++ skypeforbusinessbasic/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "configuration$architecture.xml")
 $packageName = 'Office365BusinessBasic'
 $installerType = 'EXE'
-$url = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_7213-5776.exe'
-$checksum = '50EC41172C31EA6BDABD35F633A3DC4AC7BB37D7935902EE1A1F3B1641406D68'
+$url = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_8008-3601.exe'
+$checksum = 'A7F8CD73AD61EDDB42303E7D2A0D4F4080B8330267E7B6AD63C17F12926F04DD'
 
 $silentArgs = "/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart"
 $validExitCodes = @(0)
