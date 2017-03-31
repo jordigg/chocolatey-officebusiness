2ecc57495b558000bfd44dcf709574f3db5ae74d
Jordi Garcia <jordi@improbable.io>
2017-03-31 18:24:43 +0100

    Update skype

--- skypeforbusiness/tools/chocolateyInstall.ps1
+++ skypeforbusiness/tools/chocolateyInstall.ps1
@@ -1,16 +1,12 @@
-﻿$configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'configuration.xml')
-$packageName = 'Office365Business'
+﻿$architecture = Get-ProcessorBits
+$configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "configuration$architecture.xml")
+$packageName = 'SkypeforBusinessEntryRetail'
 $installerType = 'EXE'
-$url = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_7213-5776.exe'
-$checksum = '50EC41172C31EA6BDABD35F633A3DC4AC7BB37D7935902EE1A1F3B1641406D68'
+$url = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_8008-3601.exe'
+$checksum = 'A7F8CD73AD61EDDB42303E7D2A0D4F4080B8330267E7B6AD63C17F12926F04DD'
 
 $silentArgs = "/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart"
 $validExitCodes = @(0)
-
-$architecture = Get-ProcessorBits
-If ($architecture -eq 64) {
-   $configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'configuration64.xml')
-}
 
 Write-Host "Extracting to $silentArgs"
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum $checksum -ChecksumType "sha256"
--- skypeforbusinessbasic/tools/chocolateyInstall.ps1
+++ skypeforbusinessbasic/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$architecture = Get-ProcessorBits
 $configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "configuration$architecture.xml")
-$packageName = 'Office365BusinessBasic'
+$packageName = 'SkypeforBusinessEntryRetail'
 $installerType = 'EXE'
 $url = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_8008-3601.exe'
 $checksum = 'A7F8CD73AD61EDDB42303E7D2A0D4F4080B8330267E7B6AD63C17F12926F04DD'
