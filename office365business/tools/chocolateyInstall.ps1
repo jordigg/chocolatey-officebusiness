f07a6cc4a07a3c0bfd5248452c64231d16aaf920
Jordi Garcia <jordi@improbable.io>
2017-05-21 11:58:09 +0100

    Update office deployment tool and allow language detection

--- office365business/tools/chocolateyInstall.ps1
+++ office365business/tools/chocolateyInstall.ps1
@@ -3,14 +3,15 @@ $packageName                = 'Office365Business'
 $configFile                 = Join-Path $(Split-Path -parent $script) 'configuration.xml'
 $configFile64               = Join-Path $(Split-Path -parent $script) 'configuration64.xml'
 $configurationFile          = if (Get-ProcessorBits -eq 64) { $configFile64 } else { $configFile }
+$officetempfolder           = Join-Path $env:Temp 'chocolatey\Office365Business'
 $packageArgs                = @{
     packageName             = 'Office365DeploymentTool'
     fileType                = 'exe'
-    url                     = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_7614-3602.exe'
-    checksum                = 'CB9B41ABF4C3D67D082BA534F757A0C84F7CA4AF89D77590CC58290B7C875F5E'
+    url                     = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_8008-3601.exe'
+    checksum                = 'A7F8CD73AD61EDDB42303E7D2A0D4F4080B8330267E7B6AD63C17F12926F04DD'
     checksumType            = 'sha256'
     softwareName            = 'Office365Business*'
-    silentArgs              = "/extract:$env:Temp\Office /log:$env:Temp\OfficeInstall.log /quiet /norestart"
+    silentArgs              = "/extract:`"$officetempfolder`" /log:`"$officetempfolder\OfficeInstall.log`" /quiet /norestart"
     validExitCodes          = @(
         0, # success
         3010, # success, restart required
@@ -24,16 +25,16 @@ Install-ChocolateyPackage @packageArgs
 
 # Use the deployment tool to download the setup files
 $packageArgs['packageName'] = 'Office365BusinessInstaller'
-$packageArgs['file'] = "$env:Temp\Office\Setup.exe"
-$packageArgs['silentArgs'] = "/download $configurationFile $env:temp\Office\setup.exe"
+$packageArgs['file'] = "$officetempfolder\Setup.exe"
+$packageArgs['silentArgs'] = "/download $configurationFile `"$officetempfolder\setup.exe`""
 Install-ChocolateyInstallPackage @packageArgs
 
 # Run the actual Office setup
-$packageArgs['file'] = "$env:Temp\Office\Setup.exe"
+$packageArgs['file'] = "$officetempfolder\Setup.exe"
 $packageArgs['packageName'] = $packageName
 $packageArgs['silentArgs'] = "/configure $configurationFile"
 Install-ChocolateyInstallPackage @packageArgs
 
-if (Test-Path "$env:temp\office") {
-    Remove-Item -Recurse "$env:temp\Office"
+if (Test-Path "$officetempfolder") {
+    Remove-Item -Recurse "$officetempfolder"
 }
