6bd4c581dca50ada469df4ae0b4f01a8cfd4955a
Jordi Garcia <jordi@jordigarcia.net>
2022-02-07 21:17:42 +0100

    update to v14729.20228

--- office365business/tools/chocolateyInstall.ps1
+++ office365business/tools/chocolateyInstall.ps1
@@ -130,8 +130,8 @@ else
 $packageArgs                = @{
     packageName             = 'Office365DeploymentTool'
     fileType                = 'exe'
-    url                     = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_14326-20404.exe'
-    checksum                = 'b1c39aeb91b1059e46c0e42a49e83d5346c50407159582ab07a337083d534b7c'
+    url                     = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_14729-20228.exe'
+    checksum                = 'ebb8a4843ad5e33564dbdfeea6f44c22a0df853ac65c3e17b3b3b7b5ddf6cfe2'
     checksumType            = 'sha256'
     softwareName            = 'Office365Business*'
     silentArgs              = "/extract:`"$officetempfolder`" /log:`"$officetempfolder\OfficeInstall.log`" /quiet /norestart"
